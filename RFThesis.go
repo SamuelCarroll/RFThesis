cc509458ccfcee0d4bf90b917ab987e56568c23f
samuel carroll <sam2@blackhillsinfosec.com>
2018-03-06 16:38:10 -0700

    Somehow my computer deleted the entirety of the RFThesis.go code

---
+++ RFThesis.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/SamuelCarroll/DecisionForest"
+	"github.com/SamuelCarroll/readFile"
+)
+
+func main() {
+	NUMCLASSES := 2
+	NUMTREES := 10000
+	if len(os.Args) < 3 {
+		fmt.Println("Run ./RFThesis dataFile treeFile")
+		os.Exit(-1)
+	}
+
+	inFile := os.Args[1]
+	outBase := os.Args[2]
+
+	//readFile.Read(inFile, uidPresent, classPresent)
+	myData := readFile.Read(inFile, true, true)
+
+	//DecisionForest.GenForest(allData, numClasses, numTrees, printRes, writeTrees, readTrees, outBase)
+	//Uncomment the following lines to test data
+	//fmt.Println("Testing the forest")
+	//DecisionForest.GenForest(myData, NUMCLASSES, NUMTREES, true, false, true, outBase)
+	//Uncomment the following lines to train the forest
+	fmt.Println("Training the forest")
+	DecisionForest.GenForest(myData, NUMCLASSES, NUMTREES, false, true, false, outBase)
+}
